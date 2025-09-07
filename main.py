7c94c021d04817562b16bedb0bf1a3c511ec9349
piyush goyal <piyush2022ug@gmail.com>
2025-09-07 22:04:40 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+def main():
+    if len(sys.argv) < 2:
+        print("Error: Please provide a prompt as a command line argument.")
+        sys.exit(1)
+    
+    prompt = sys.argv[1]
+    
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=prompt
+    )
+    
+    print(response.text)
+    
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+if __name__ == "__main__":
+    main()
